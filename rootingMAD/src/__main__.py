0424217cd89bf61af4dd7aaead2806f7adb452bd
marisaKoe <marisa.koellner@student.uni-tuebingen.de>
2019-08-15 14:02:20 +0200

    added code for ml analysis

--- rootingMAD/src/__main__.py
+++ rootingMAD/src/__main__.py
@@ -14,7 +14,9 @@ def MAD_rooting_treesample(method):
     the output is written in the same folder with the ending .rooted
     :param method: the name of the method to get the right trees
     '''
-    path = glob.glob("/home/marisa/Dropbox/EVOLAEMP/projects/Project-Borrowing-hgt/rootingMAD/NELex/"+method+"/*.nwk")
+    ##pmi path
+    #path = glob.glob("/home/marisa/Dropbox/EVOLAEMP/projects/Project-Borrowing-hgt/rootingMAD/NELex/"+method+"/*.nwk")
+    path = glob.glob("/home/marisakoe/Dropbox/EVOLAEMP/projects/Project-ConceptTrees-CharacterBased/NELex/ML_iqtree/NgramsNW/bootstrapReplicates/*.boottrees")
     for filename in path:
         p = subprocess.Popen('mad '+filename+' -n',shell=True)
         os.waitpid(p.pid,0)
@@ -26,25 +28,32 @@ def MAD_rooting_singleTree(path):
     the output is written in the same folder with the ending .rooted
     :param method: the name of the method to get the right trees
     '''
-    path = glob.glob(path+"*.nwk")
+    ###pmi = *.nwk | iqtree = *.treefile
+    path = glob.glob(path+"*.treefile")
     for filename in path:
         p = subprocess.Popen('mad '+filename+' -n',shell=True)
         os.waitpid(p.pid,0)
 
 if __name__ == '__main__':
+    
+    ###pmi fastme trees (single trees)
+    #method = "PMI_multipleData"
+    #inpath = "/home/marisakoe/Dropbox/EVOLAEMP/projects/Project-ConceptTrees-DistanceMethods/NELex/PMI_based_methods/PMI_multipleData/phylipTrees/fastme/*.nwk"
+    #outpath = "/home/marisakoe/Dropbox/EVOLAEMP/projects/Project-ConceptTrees-DistanceMethods/NELex/PMI_based_methods/PMI_multipleData_rootedTrees/"
+    #edit_negativeBL.edit_single_tree(inpath, outpath)
+    ####iqtree
+    outpath = "/home/marisakoe/Dropbox/EVOLAEMP/projects/Project-ConceptTrees-CharacterBased/NELex/ML_iqtree/NgramsNW/iqTrees/"
+    MAD_rooting_singleTree(outpath)
+    
     ##only do for the optimal methods, since they are used for the HGT transfer
     ##pmi bootstrapping with Noise
     #methodBS = "pmiMultidata"
     #pathBS = "/home/marisa/Dropbox/EVOLAEMP/projects/Project-BootstrappingWithNoise/nelex/"+methodBS+"/trees/*.nwk"
     #edit_negativeBL.edit_treesample(methodBS, pathBS)
     #MAD_rooting_treesample(methodBS)
-    
-    ###pmi fastme trees (single trees)
-    #method = "PMI_multipleData"
-    inpath = "/home/marisakoe/Dropbox/EVOLAEMP/projects/Project-ConceptTrees-DistanceMethods/NELex/PMI_based_methods/PMI_multipleData/phylipTrees/fastme/*.nwk"
-    outpath = "/home/marisakoe/Dropbox/EVOLAEMP/projects/Project-ConceptTrees-DistanceMethods/NELex/PMI_based_methods/PMI_multipleData_rootedTrees/"
-    #edit_negativeBL.edit_single_tree(inpath, outpath)
-    MAD_rooting_singleTree(outpath)
+    ####iqtree
+    methodBS = "ML_ngramsNW"
+    MAD_rooting_treesample(methodBS)
     
     
     
